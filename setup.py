f472602769bf1c9b9526a6d75dab33b3e6655198
Luka Stojanaovic <luka.stojanovic@totient.bio>
2020-08-13 23:00:33 +0200

    setup fix

--- setup.py
+++ setup.py
@@ -3,12 +3,14 @@ setup(
     name="GAtNN-VS",
     version="1.0",
     packages=find_packages(),
-    install_requires=["pytorch>=1.4", "dgl>=0.4.0", "rdkit", "tqdm", "hydra-core", "omegaconf", "pandas", "cytoolz", "feather-format"],
+    install_requires=[
+        "pytorch>=1.4", "dgl>=0.4.0", "rdkit", 
+        "tqdm", "hydra-core", "omegaconf", "pandas", "cytoolz", "feather-format"],
 
     # metadata to display on PyPI
-    author="Totient Inc",
+    author="Totient inc.",
     author_email="me@example.com",
-    description="This is an Example Package",
+    description="Neural network based virtual screening toolkit",
     keywords="graph,attention,neural network,chemistry,virtual screen",
     url="https://github.com/totient-bio/gatnn-vs",
     project_urls={
@@ -16,7 +18,7 @@ setup(
         "Documentation": "https://github.com/totient-bio/gatnn-vs",
         "Source Code": "https://github.com/totient-bio/gatnn-vs",
     },
-    data_files = [("config", ["train-config.yaml"])]
+    data_files = [("config", ["train-config.yaml", "eval-config.yaml"])],
     classifiers=[
         "Development Status :: 4 - Beta",
         "Environment :: Console",
