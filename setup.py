fa4e3bc42ff9d0c43f0b3febe644a437bd7c468a
Luka Stojanaovic <luka.stojanovic@totient.bio>
2020-08-13 12:40:23 +0200

    README and setup.py

---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+setup(
+    name="GAtNN-VS",
+    version="1.0",
+    packages=find_packages(),
+    install_requires=["pytorch>=1.4", "dgl>=0.4.0", "rdkit", "tqdm", "hydra-core", "omegaconf", "pandas", "cytoolz"],
+
+    # metadata to display on PyPI
+    author="Me",
+    author_email="me@example.com",
+    description="This is an Example Package",
+    keywords="graph attention neural network chemistry virtual screen",
+    url="https://github.com/totient-bio/gatnn-vs",
+    project_urls={
+        "Bug Tracker": "https://github.com/totient-bio/gatnn-vs/issues",
+        "Documentation": "https://github.com/totient-bio/gatnn-vs",
+        "Source Code": "https://github.com/totient-bio/gatnn-vs",
+    },
+    data_files = [("config", ["train-config.yaml"])]
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Console",
+        "Environment :: GPU :: NVIDIA CUDA",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        "Topic :: Scientific/Engineering :: Chemistry"
+    ],
+    python_requires=">=3.6"
+)
